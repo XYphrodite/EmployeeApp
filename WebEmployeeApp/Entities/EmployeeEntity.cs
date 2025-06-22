00fafdc9a63059ec0962f2a4dad0c8361458c0e8
mrmak <mrmak@XY-MOBDESK-R7>
2025-06-22 15:43:25 +0300

    Add project files.

---
+++ WebEmployeeApp/Entities/EmployeeEntity.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeApp.Server.Models;
+public class Employee
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Firstname { get; set; }
+
+    [Required]
+    public string Surname { get; set; }
+
+    public string? Lastname { get; set; }
+
+    public DateTime? Birthday { get; set; }
+
+    [Required]
+    public short PositionId { get; set; }
+
+    [ForeignKey("PositionId")]
+    public Position Position { get; set; }
+
+    [Required]
+    public int Salary { get; set; }
+
+    [Required]
+    public bool IsActive { get; set; }
+}
+
+
+
+
---
+++ WebEmployeeApp/Entities/PositionEntity.cs
@@ -0,0 +1,11 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+public class Position
+{
+    [Key]
+    public short Id { get; set; }
+
+    [Required]
+    public string PositionName { get; set; }
+}
